86bf092e9cdfd495b671b91a8f133a3e3679fe43
Leoooo13 <150640324+Leoooo13@users.noreply.github.com>
2024-06-03 11:29:16 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,3 @@
-/* MoveNet Skeleton - Steve's Makerspace (most of this code is from TensorFlow)
-
-MoveNet is developed by TensorFlow:
-https://www.tensorflow.org/hub/tutorials/movenet
-
-*/
-
 let video, bodypose, pose, keypoint, detector; // 定義變量
 let poses = [];
 let img; // 用於存放您的物件圖片
@@ -57,6 +50,7 @@ async function setup() {
 function draw() {
   image(video, 0, 0); // 繪製視頻到畫布上
   drawSkeleton(); // 繪製骨架
+  drawOverlay(); // 繪製圖片和文字
   // 水平翻轉圖像
   cam = get();
   translate(cam.width, 0);
@@ -65,51 +59,79 @@ function draw() {
 }
 
 // 繪製骨架
-function drawSkeleton() {
+// function drawSkeleton() {
+//   for (let i = 0; i < poses.length; i++) {
+//     pose = poses[i];
+
+//     // 繪製肩膀到手腕的線條
+//     for (let j = 5; j < 9; j++) {
+//       if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
+//         let partA = pose.keypoints[j];
+//         let partB = pose.keypoints[j + 2];
+//         line(partA.x, partA.y, partB.x, partB.y); // 繪製線條
+//       }
+//     }
+
+//     // 繪製肩膀到肩膀的線條
+//     let partA = pose.keypoints[5];
+//     let partB = pose.keypoints[6];
+//     if (partA.score > 0.1 && partB.score > 0.1) {
+//       line(partA.x, partA.y, partB.x, partB.y); // 繪製線條
+//     }
+
+//     // 繪製髖部到髖部的線條
+//     partA = pose.keypoints[11];
+//     partB = pose.keypoints[12];
+//     if (partA.score > 0.1 && partB.score > 0.1) {
+//       line(partA.x, partA.y, partB.x, partB.y); // 繪製線條
+//     }
+
+//     // 繪製肩膀到髖部的線條
+//     partA = pose.keypoints[5];
+//     partB = pose.keypoints[11];
+//     if (partA.score > 0.1 && partB.score > 0.1) {
+//       line(partA.x, partA.y, partB.x, partB.y); // 繪製線條
+//     }
+//     partA = pose.keypoints[6];
+//     partB = pose.keypoints[12];
+//     if (partA.score > 0.1 && partB.score > 0.1) {
+//       line(partA.x, partA.y, partB.x, partB.y); // 繪製線條
+//     }
+
+//     // 繪製髖部到腳部的線條
+//     for (let j = 11; j < 15; j++) {
+//       if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
+//         partA = pose.keypoints[j];
+//         partB = pose.keypoints[j + 2];
+//         line(partA.x, partA.y, partB.x, partB.y); // 繪製線條
+//       }
+//     }
+//   }
+// }
+
+// 繪製圖片和文字
+function drawOverlay() {
   for (let i = 0; i < poses.length; i++) {
     pose = poses[i];
 
-    // 繪製肩膀到手腕的線條
-    for (let j = 5; j < 9; j++) {
-      if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
-        let partA = pose.keypoints[j];
-        let partB = pose.keypoints[j + 2];
-        line(partA.x, partA.y, partB.x, partB.y); // 繪製線條
-      }
+    // 在耳朵位置繪製並移動物件圖片
+    let leftEar = pose.keypoints[3];
+    let rightEar = pose.keypoints[4];
+    if (leftEar.score > 0.1) {
+      displayMovingImage(leftEar, img, 1); // 左耳
+    }
+    if (rightEar.score > 0.1) {
+      displayMovingImage(rightEar, img, 1); // 右耳
     }
 
-    // 繪製肩膀到肩膀的線條
-    let partA = pose.keypoints[5];
-    let partB = pose.keypoints[6];
-    if (partA.score > 0.1 && partB.score > 0.1) {
-      line(partA.x, partA.y, partB.x, partB.y); // 繪製線條
+    // 在手腕位置繪製並移動物件圖片
+    let leftWrist = pose.keypoints[9];
+    let rightWrist = pose.keypoints[10];
+    if (leftWrist.score > 0.1) {
+      displayMovingImage(leftWrist, img, -1); // 左手腕
     }
-
-    // 繪製髖部到髖部的線條
-    partA = pose.keypoints[11];
-    partB = pose.keypoints[12];
-    if (partA.score > 0.1 && partB.score > 0.1) {
-      line(partA.x, partA.y, partB.x, partB.y); // 繪製線條
-    }
-
-    // 在眼睛位置繪製物件圖片
-    let leftEye = pose.keypoints[1];
-    let rightEye = pose.keypoints[2];
-    if (leftEye.score > 0.1) {
-      image(img, leftEye.x - 25, leftEye.y - 25, 50, 50); // 繪製圖片
-    }
-    if (rightEye.score > 0.1) {
-      image(img, rightEye.x - 25, rightEye.y - 25, 50, 50); // 繪製圖片
-    }
-
-    // 在肩膀位置繪製物件圖片
-    let leftShoulder = pose.keypoints[5];
-    let rightShoulder = pose.keypoints[6];
-    if (leftShoulder.score > 0.1) {
-      image(img, leftShoulder.x - 25, leftShoulder.y - 25, 50, 50); // 繪製圖片
-    }
-    if (rightShoulder.score > 0.1) {
-      image(img, rightShoulder.x - 25, rightShoulder.y - 25, 50, 50); // 繪製圖片
+    if (rightWrist.score > 0.1) {
+      displayMovingImage(rightWrist, img, -1); // 右手腕
     }
 
     // 在頭頂上方顯示學號和姓名
@@ -120,46 +142,17 @@ function drawSkeleton() {
       textAlign(CENTER); // 設置文字對齊方式為中心對齊
       text(`${studentID} ${studentName}`, nose.x, nose.y - 50); // 顯示學號和姓名
     }
-
-    // 繪製肩膀到髖部的線條
-    partA = pose.keypoints[5];
-    partB = pose.keypoints[11];
-    if (partA.score > 0.1 && partB.score > 0.1) {
-      line(partA.x, partA.y, partB.x, partB.y); // 繪製線條
-    }
-    partA = pose.keypoints[6];
-    partB = pose.keypoints[12];
-    if (partA.score > 0.1 && partB.score > 0.1) {
-      line(partA.x, partA.y, partB.x, partB.y); // 繪製線條
-    }
-
-    // 繪製髖部到腳部的線條
-    for (let j = 11; j < 15; j++) {
-      if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
-        partA = pose.keypoints[j];
-        partB = pose.keypoints[j + 2];
-        line(partA.x, partA.y, partB.x, partB.y); // 繪製線條
-      }
-    }
   }
 }
 
-/* Points (view on left of screen = left part - when mirrored)
-  0 nose
-  1 left eye
-  2 right eye
-  3 left ear
-  4 right ear
-  5 left shoulder
-  6 right shoulder
-  7 left elbow
-  8 right elbow
-  9 left wrist
-  10 right wrist
-  11 left hip
-  12 right hip
-  13 left knee
-  14 right knee
-  15 left foot
-  16 right foot
-*/
+// 繪製並移動圖片
+function displayMovingImage(keypoint, img, direction) {
+  let x = keypoint.x + frameCount * 0.5 * direction;
+  let y = keypoint.y;
+  if (direction === 1) {
+    if (x > width) x = 0;
+  } else {
+    if (x < 0) x = width;
+  }
+  image(img, x - 25, y - 25, 50, 50); // 繪製圖片
+}
